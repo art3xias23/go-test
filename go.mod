10bc94a6fa364c81066f4d568d806ab917808574
Tino <Konstantin.v.milchev@gmail.com>
2024-04-16 09:16:37 +0100

    added templ with chi

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/art3xias23/go-test
 go 1.22.2
 
 require github.com/go-chi/chi/v5 v5.0.12
+
+require github.com/a-h/templ v0.2.663
--- main.go
+++ main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"net/http"
 
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/a-h/templ"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
@@ -14,8 +15,12 @@ func main() {
 		w.Write([]byte("Hello World"))
 	})
 	r.Get("/test", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("test"))
+		w.Write([]byte("Hello"))
 	})
 
+	r.Get("/templ", templ.Handler(Hell("MyBoy")).ServeHTTP)
+
+
 	http.ListenAndServe(":3000", r)
+
 }
